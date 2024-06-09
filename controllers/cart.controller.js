097ac9a5c370fa1107846fedbf9ee00347e1c2d1
shanmukh-sai2003 <koradashanmukhasai10@gmail.com>
2024-06-09 11:04:23 +0530

    Fix add item

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,11 +1,12 @@
 import Cart from '../models/cart.js';
+import order from '../models/order.js';
 import Order from '../models/order.js';
 
 export const getAllItems = async (req, res) => {
     try {
         const userId = req.user.userInfo.userId;
         const itemsList = await Cart.find({ userId: userId }).populate('orders');
-
+        console.log(itemsList);
         return res.status(200).json({ success: true, data: itemsList.orders });
     } catch (error) {
         console.log(error.message);
@@ -25,7 +26,9 @@ export const addItemToCart = async (req, res) => {
         const cartId = cart._id;
         const { product, price } = req.body;
         const newOrder = new Order({ userId: userId, cartId: cartId, product: product, price: price });
+        
         cart.orders.push(newOrder._id);
+        await newOrder.save();
         await cart.save();
 
         return res.status(200).json({ success: true, message: 'item added to cart' });
