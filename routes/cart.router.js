0cfff7eb781141567e547b2874c60fe37bab9726
shanmukh-sai2003 <koradashanmukhasai10@gmail.com>
2024-06-09 10:30:22 +0530

    Add verify access token middleware

--- routes/cart.router.js
+++ routes/cart.router.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { placeOrder } from '../controllers/order.controller.js';
 import { addItemToCart, removeItem } from '../controllers/cart.controller.js';
+import { verifyAccessToken } from '../middlewares/auth.js';
 
 const router = express.Router();
 
-router.post('', addItemToCart);
-router.post('/checkOut', placeOrder);
-router.delete('/:orderId', removeItem);
+router.post('', verifyAccessToken, addItemToCart);
+router.post('/checkOut', verifyAccessToken, placeOrder);
+router.delete('/:orderId', verifyAccessToken, removeItem);
 
 export default router
